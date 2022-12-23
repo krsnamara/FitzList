c098d25ce6b0df001913b0ed7348bda72ad2d747
krsnamara <martinj.fitzpatrick@gmail.com>
2022-12-23 00:42:07 -0500

    mongoDB connection made, server.js linked to main controller

---
+++ controllers/main.js
@@ -0,0 +1,17 @@
+// Dependencies
+
+// I is for INDEX
+
+// N is for NEW
+
+// D is for DELETE
+
+// U is for UPDATE
+
+// C is for CREATE
+
+// E is for EDIT
+
+// S is for SHOW
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,40 @@
+// Dependencies
 const express = require('express');
 const app = express();
-require('dotenv').config();
 const mongoose = require('mongoose');
+require('dotenv').config();
 
+// DB config
+mongoose.connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+});
+
+// Database Connection Error / Success
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err.message + ' is mongodb not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Middlewares
+app.use(express.urlencoded({ extended: false }));
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+// Controllers
+const mainController = require('./controllers/main');
+app.use('/main', mainController);
+
+// Routes
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+});
+
+// Shallow route to the route for comment delete function
+// app.use('/', articlesController)
+
+// Listening 
+app.listen(3000, () =>{
+    console.log('listening...')
+});
